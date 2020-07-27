c60f34129a7f6b02576b4080379ab44c9f2d5d1e
abdelhak.ajbouni <abdelhak.ajbouni@gmail.com>
2020-07-27 21:40:25 +0100

    WIP: landing page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import LandingPage from "../landing-page";
+
 import "./app.scss";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app bg-gray-200">
+      <LandingPage onSearch={console.log} />
+    </div>
+  );
 }
 
 export default App;
